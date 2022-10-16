1ab629f43936f0d22532c4b7ac915efaccd5cbe8
Roshan More <102913351+Roshan779846@users.noreply.github.com>
2022-10-16 15:10:04 +0530

    Add files via upload
    This is the first working app commit

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,11 @@
+import { FaPauseCircle,FaPlayCircle} from 'react-icons/fa';
+
+
+const PlayPause = ({ isplaying, activeSong, song, handlePause, handlePlay }) =>
+  isplaying && activeSong?.title === song.title ? (
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle size={35} className="text-gray-300" onClick={handlePlay} />
+  );
+
+export default PlayPause;
